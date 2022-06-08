f37fad214ad93eb37f3a6bd65ed8f267d38a7e20
AngelaJiaNan <angelanan12@gmail.com>
2022-06-08 13:58:04 -0700

    updated routes to include POST endpoint to appointments.

--- app/controllers/api/appointments_controller.rb
+++ app/controllers/api/appointments_controller.rb
@@ -1,8 +1,5 @@
 class Api::AppointmentsController < ApplicationController
   def index
-
-    @appointments = []
-
     if params[:past] != "0" && params[:past] != "1"
       render json: {status: "error", code: 422, message: "Query parameters can only equal 0 and 1."}
     elsif params[:past] == "1"
@@ -11,26 +8,29 @@ class Api::AppointmentsController < ApplicationController
       appointments = Appointment.where("start_time > ?", Time.zone.now)
     elsif params[:page] > 0 && params[:length] > 0
       offset = params[:page] - 1 * params[:length]
-      p "Offset is #{offset}"
       appointments = Appointment.offset(offset).limit(params[:length])
     else
       appointments = Appointment.all
     end
-      appointments.each do |a|
-        appointments.push({
-          id: a.id,
-          patient: { name: a.patient.name },
-          doctor: { name: a.doctor.name, id: a.doctor.id },
-          created_at: a.created_at,
-          start_time: a.start_time,
-          duration_in_minutes: a.duration_in_minutes
-        })
-      end
-      render json: appointments
+      # appointments.each do |a|
+      #   appointments.push({
+      #     id: a.id,
+      #     patient: { name: a.patient.name },
+      #     doctor: { name: a.doctor.name, id: a.doctor.id },
+      #     created_at: a.created_at,
+      #     start_time: a.start_time,
+      #     duration_in_minutes: a.duration_in_minutes
+      #   })
+      # end
+    render json: appointments
   end
 
   def create
-
-
+    appointment = Appointment.new(
+      patient_id: Patient.where(name: params[:patient][:name]),
+      doctor_id: params[:doctor][:id],
+      duration_in_minutes: params[:duration_in_minutes],
+      start_time: params[:start_time]
+    )
   end
 end
--- spec/controllers/api/appointments_controller_spec.rb
+++ spec/controllers/api/appointments_controller_spec.rb
@@ -2,10 +2,8 @@ RSpec.describe Api::AppointmentsController do
 
   describe "#index" do
     it "returns json data" do
-      get :index, format: :json
-      p response.body
-      p response.status
-      expect(response.status).to eq(200)
+      get :index
+      expect(response.status).to eq(204)
     end
   end
 end
